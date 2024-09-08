090b75545f9402d27b29665347c14640c9faea3c
Siggi <7286178+Siggi88@users.noreply.github.com>
2024-09-08 20:32:07 +0000

    Convert JNI code from C to C++

--- jni/io_siggi_reflectionfreedom_ReflectionFreedom.cpp
+++ jni/io_siggi_reflectionfreedom_ReflectionFreedom.cpp
@@ -2,20 +2,20 @@
 
 JNIEXPORT void JNICALL Java_io_siggi_reflectionfreedom_ReflectionFreedom_setAccessible
   (JNIEnv *env, jclass clazz, jobject accessibleObject, jboolean accessible) {
-	jclass aoClass = (*env)->GetObjectClass(env, accessibleObject);
-	jfieldID overrideField = (*env)->GetFieldID(env, aoClass, "override", "Z");
-	(*env)->SetBooleanField(env, accessibleObject, overrideField, accessible);
+	jclass aoClass = env->GetObjectClass(accessibleObject);
+	jfieldID overrideField = env->GetFieldID(aoClass, "override", "Z");
+	env->SetBooleanField(accessibleObject, overrideField, accessible);
 }
 
 JNIEXPORT void JNICALL Java_io_siggi_reflectionfreedom_ReflectionFreedom_setModifiers
   (JNIEnv *env, jclass clazz, jobject accessibleObject, jint modifiers) {
-	jclass aoClass = (*env)->GetObjectClass(env, accessibleObject);
-	jfieldID modifierField = (*env)->GetFieldID(env, aoClass, "modifiers", "I");
-	(*env)->SetIntField(env, accessibleObject, modifierField, modifiers);
+	jclass aoClass = env->GetObjectClass(accessibleObject);
+	jfieldID modifierField = env->GetFieldID(aoClass, "modifiers", "I");
+	env->SetIntField(accessibleObject, modifierField, modifiers);
 }
 
 JNIEXPORT jobject JNICALL Java_io_siggi_reflectionfreedom_ReflectionFreedom_allocateObject
   (JNIEnv *env, jclass clazz, jclass classToAllocate) {
-	return (*env)->AllocObject(env, classToAllocate);
+	return env->AllocObject(classToAllocate);
 }
 
